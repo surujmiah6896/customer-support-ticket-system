1c61d53d026f920086bbbacf22eff254210f5409
surujmiah6896 <surujmiah6896@gmail.com>
2025-10-19 21:13:43 +0600

    create comment with tickets

--- support-ticket-backend/app/Http/Controllers/CommentController.php
+++ support-ticket-backend/app/Http/Controllers/CommentController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, $ticketId)
+    {
+        $ticket = Ticket::forUser($request->user())->find($ticketId);
+
+        if(!$ticket){
+            return response()->json(['errors' => ['message'=>'ticket not found!']], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'content' => 'required|string|max:1000',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $comment = Comment::create([
+            'content' => $request->content,
+            'ticket_id' => $ticketId,
+            'user_id' => $request->user()->id,
+        ]);
+
+        $data = [
+            'status' => true,
+            'comment' => $comment->load('user'),
+            'messages' => 'create comment successfully',
+        ];
+
+        return response()->json($data, 201);
+    }
 }
--- support-ticket-backend/routes/api.php
+++ support-ticket-backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,10 +18,13 @@ Route::prefix('v1')->group(function(){
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/user', [AuthController::class, 'user']);
 
-
+        // tickets
         Route::apiResource('tickets', TicketController::class);
         Route::post('tickets/{id}', [TicketController::class,'update']);
         Route::delete('tickets/{id}', [TicketController::class, 'destroy']);
+
+        //comments
+        Route::post('tickets/{ticketId}/comments', [CommentController::class, "store"]);
     });
 });
 
